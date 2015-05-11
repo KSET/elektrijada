07fc278c2c574be995826465fc5b690f9c731300
Andro Rezic <andro.rezic@gmail.com>
2015-05-11 15:26:13 +0200

    Added tos to cv

--- zivotopis/forms.py
+++ zivotopis/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.utils.timezone import now
 from zivotopis.models import CurriculumVitae
 
 
@@ -10,6 +11,18 @@ class CVForm(forms.ModelForm):
                                   error_messages={'invalid': u'Unesite ispravanu godinu od 4 znamenke (bez točke).'})
     phone = forms.RegexField(label=u'Broj mobitela', regex=r'^\d+$', required=False,
                              error_messages={'invalid': u'Unesite ispravan broj (samo znamenke).'})
+    tos = forms.BooleanField(label=u'Slažem se da se moj životopis da na uvid srebrnim, zlatnim i platinastim '
+                             'partnerima natjecatelja FER-a na Elektrijadi', required=True, widget=forms.CheckboxInput)
+
+    def __init__(self, *args, **kwargs):
+        super(CVForm, self).__init__(*args, **kwargs)
+        self.fields['tos'].label += ' %d.' % now().year
+        self.fields['email'].widget.attrs['type'] = 'email'
+        self.fields['phone'].widget.attrs['type'] = 'phone'
+        for name, field in self.fields.items():
+            if field.required:
+                field.widget.attrs['required'] = ''
+                field.label += '*'
 
     class Meta:
         model = CurriculumVitae
